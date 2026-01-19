3838f50d0d01b0338025ae367898755f7cf4ee0a
Sabir <magsabir95@gmail.com>
2026-01-19 21:28:36 +0300

    feat(quiz): add queue module with Redis connection

---
+++ src/modules/quiz/queue/game.processor.ts
@@ -0,0 +1,7 @@
+import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from 'bullmq';
+
+@Processor('game')
+export class GameProcessor extends WorkerHost {
+  async process(job: Job) {}
+}
